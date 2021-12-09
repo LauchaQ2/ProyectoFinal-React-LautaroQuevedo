406e586f12377701f4ad8d04f72cbb6017a035ce
Lautaro Quevedo <lautaroquevedocalzada@gmail.com>
2021-12-09 20:17:12 -0300

    Update ItemDetail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -8,14 +8,14 @@ export default function ItemDetail({data}){
     return(
 
         <div className="container mb-2 mt-2">
-            <div className="row">
-                <div className="col-6 text-center">
+            <div className="row ">
+                <div className="col-6 detail text-center">
                     <img className="img-fluid" src={data[0].pictureUrl} />
                 </div>
-                <div className="col-6 align-content-between text-left">
+                <div className="col-6 detail justify-content-center align-content-between text-center">
                     <h1>{data[0].title}</h1>
                     <h5>Tipo de libro: {data[0].description}.</h5>
-                    <h3>Precio {data[0].price}.</h3>
+                    <h3>Precio: ${data[0].price}.</h3>
                     <ItemCount stock="5"/>
                 </div>
             </div>
