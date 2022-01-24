be2deb122cbd3256c93a607c2b61c6250c6eab37
Lautaro Quevedo <lautaroquevedocalzada@gmail.com>
2022-01-24 16:10:57 -0300

    Add login  validation

--- src/components/ModalLogin/ModalLogin.js
+++ src/components/ModalLogin/ModalLogin.js
@@ -6,17 +6,19 @@ import DialogContent from '@mui/material/DialogContent';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import db from '../../firebaseconfig';
-import {doc, collection, addDoc, setDoc  } from 'firebase/firestore';
+import {doc,getDoc, collection, addDoc, setDoc  } from 'firebase/firestore';
 import CartContext from '../../context/CartContext';
 import '../Checkout/Checkout.css'
 import './ModalLogin.css'
 import { Link } from 'react-router-dom';
+
 
 export default function ModalLogin({products,showCart}){
       
 
     const {logged, setLogged, username, setUsername} = useContext(CartContext)
     const [noUser, setNoUser] = useState(false)
+    const [userIncorrect, setUserIncorrect] = useState(false)
 
     const [size, setSize] = useState(window.innerWidth);
     
@@ -31,16 +33,41 @@ export default function ModalLogin({products,showCart}){
        usuario : '',
        contraseña : '',
    })
+   const [userRegistered, setUserRegistered] = useState({
+    usuario : '',
+    contraseña : '',
+})
    const [orderId, setOrderId] = useState(null)
 
    
-
+    var id = userRegistered.usuario;
 
    const handleChange = (e) => {
        const {name, value} = e.target
        setNewUser({...newUser, [name] : value})
+       setUserRegistered({...userRegistered, [name] : value})
        console.log(newUser)
    }
+
+   async function getUser(){
+    const docRef = doc(db, "users", id);
+    const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+        }
+        if(userRegistered.usuario === docSnap.data().usuario && userRegistered.contraseña === docSnap.data().contraseña){
+            setLogged(true)
+        }
+        else{
+            console.log("No coinciden")
+            setUserIncorrect(true);
+        }
+    }
+
 
 
 
@@ -62,6 +89,7 @@ export default function ModalLogin({products,showCart}){
 
    const closeSession = () =>{
        setLogged(false)
+       setUserIncorrect(false)
    }
     
     return(
@@ -77,8 +105,13 @@ export default function ModalLogin({products,showCart}){
                             <Box className={size > 500 ? "form-container" : "w-100 form-mobile text-center"} noValidate autoComplete="off" >
                                 <TextField className={size > 500 ? null : "w-100"} label="Usuario" required type="text" name="usuario" variant="outlined" value={newUser.usuario.trim()} onChange={handleChange}/>
                                 <TextField className={size > 500 ? null : "w-100"} label="Contraseña" required type="password" name="contraseña" variant="outlined" value={newUser.contraseña} onChange={handleChange}/>
+                                { userIncorrect ?
+                                    <span className={size < 500 ? "mb-3 fs8-red" :' fs8-red mb-3'}>Usuario o contraseña incorrecta</span>
+                                    :
+                                    null
+                                    }
                                 <span onClick={() => {setNoUser(true)}} className={size < 500 ? "mb-3 fs-6" :'mb-3'}>¿No tienes cuenta? Registrate aquí</span>
-                            <Button variant="outlined" onClick={sendOrder}>Ingresar</Button>
+                            <Button variant="outlined" onClick={getUser}>Ingresar</Button>
                             </Box>
                     </DialogContent>
                     </>
